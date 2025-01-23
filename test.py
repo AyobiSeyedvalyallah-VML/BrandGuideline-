1cadce1dfe623cde69f41d50eca0b75fbd891fbd
AyobiSeyedvalyallah-VML <115994143+AyobiSeyedvalyallah-VML@users.noreply.github.com>
2025-01-23 15:53:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ st.session_state.setdefault("extract", True)
 st.session_state.setdefault("df", '')
 st.session_state.setdefault("retry",True)
 st.session_state.setdefault('filename','')
+st.session_state.setdefault('process',False)
 file_name = ''
 df = ''
 for uploaded_file in uploaded_files:
@@ -29,85 +30,98 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     
     html_content = bytes_data.decode('utf-8')
-    if uploaded_file.name is not None:
-        st.session_state['filename'] = uploaded_file.name
-    # st.write(uploaded_file.name)
-    if st.session_state['extract']:
-        file_name = uploaded_file.name
-        with st.spinner("Extracting data..."):
+    if st.button("Generate configuration"):
+            with st.spinner("Generating..."):
+                configuration = config(html_content)
+                st.write(configuration)
+    
+    if  st.button("Preview"):
+            if uploaded_file.name is not None:
+                st.session_state['filename'] = uploaded_file.name
+            # st.write(uploaded_file.name)
+            if st.session_state['extract']:
+                file_name = uploaded_file.name
+                with st.spinner("Extracting data..."):
+                    try:
+                        df = prompt_transformer(html_content).replace('json','').strip()
+                    except:
+                        df = prompt_transformer(html_content).replace('json','').strip()
+                    st.session_state['df'] = df
+                    st.session_state['extract']=False
+                    # st.snow()
+                
+            count = 0
             try:
-                df = prompt_transformer(html_content).replace('json','').strip()
+                df = json.loads(st.session_state['df'])
+                df = pd.json_normalize(df)
+                st.session_state['retry'] = False
+                st.session_state['process'] = True
+                # df = pd.read_csv(StringIO(st.session_state['df']),sep='**',header=0)
             except:
-                df = prompt_transformer(html_content).replace('json','').strip()
-            st.session_state['df'] = df
-            st.session_state['extract']=False
-            # st.snow()
-        
-    count = 0
-    try:
+                if st.session_state['retry']:
+                        with st.spinner("Retry: Extracting data..."):
+                            try:
+                                time.sleep(5)
+                                df = fix_csv(st.session_state['df'])
+                                df = json.loads(st.session_state['df'])
+                                df = pd.json_normalize(df)
+                                # df = pd.read_csv(StringIO(df),sep='**',header=0)
+                                st.session_state['retry'] = False
+                                st.session_state['process'] = True
+                                # st.session_state['df'] = df
+                            except Exception as e:
+                                # df = prompt_transformer(html_content)
+                                # df = pd.read_csv(StringIO(st.session_state['df']),sep='\t',header=0)
+                                # st.session_state['df'] = df
+                                st.write('Error: ',"can't parse the file")
+            # if df.columns[0] == 'plaintext':
+            # st.write(st.session_state['df'])
+            # f = pd.read_csv(StringIO(st.session_state['df']),sep='\t',header=0)
+            # st.write(f)
+            # df.columns = ['image_link', 'description', 'category']
+            # st.write(df.columns)
+            # if st.button("Generate configuration"):
+            #     with st.spinner("Generating..."):
+            #         configuration = config(html_content)
+            #         st.write(configuration)
+                
+    if st.session_state['process']:
         df = json.loads(st.session_state['df'])
         df = pd.json_normalize(df)
-        # df = pd.read_csv(StringIO(st.session_state['df']),sep='**',header=0)
-    except:
-       if st.session_state['retry']:
-            with st.spinner("Retry: Extracting data..."):
-                try:
-                    time.sleep(5)
-                    df = fix_csv(st.session_state['df'])
-                    df = json.loads(st.session_state['df'])
-                    df = pd.json_normalize(df)
-                    # df = pd.read_csv(StringIO(df),sep='**',header=0)
-                    st.session_state['retry'] = False
-                    # st.session_state['df'] = df
-                except Exception as e:
-                    # df = prompt_transformer(html_content)
-                    # df = pd.read_csv(StringIO(st.session_state['df']),sep='\t',header=0)
-                    # st.session_state['df'] = df
-                    st.write('Error: ',"can't parse the file")
-    # if df.columns[0] == 'plaintext':
-    # st.write(st.session_state['df'])
-    # f = pd.read_csv(StringIO(st.session_state['df']),sep='\t',header=0)
-    # st.write(f)
-    # df.columns = ['image_link', 'description', 'category']
-    # st.write(df.columns)
-    if st.button("Generate configuration"):
-        with st.spinner("Generating..."):
-            configuration = config(html_content)
-            st.write(configuration)
+        choice = st.selectbox(
+                "Select the Type",
+                [i for i in df['category'].unique() if i != 'category' and i != 'plaintext'],
+                index=None,
+                placeholder="Please select the category..."
+            )
+        df.to_csv('output.csv')
+        temp = df[df['category']==choice]
+        cols = st.columns(3)
+        # st.write(temp.columns)
+        for index,row in temp.iterrows():
+            with cols[index % 3]:
+                if 'https' not in row['image_link']:
+                    st.image(f"https://spectrum.adobe.com{row['image_link']}", caption=row['description'],use_container_width =False)
+                else:
+                    st.image(row['image_link'], caption=row['description'],use_container_width =False)
+        file_name = capitalize_if_needed(st.session_state['filename'].replace(".html",""))
+        # fonts_folder = os.path.join(project_path,'fonts')
+        fonts_path = os.path.join(fonts_folder,file_name)
+        fonts = os.listdir(fonts_path)
+        #fonts = os.listdir(f'./fonts/{file_name}')
         
-    choice = st.selectbox(
-        "Select the Type",
-        [i for i in df['category'].unique() if i != 'category' and i != 'plaintext'],
-        index=None,
-        placeholder="Please select the category..."
-    )
-    df.to_csv('output.csv')
-    temp = df[df['category']==choice]
-    cols = st.columns(3)
-    # st.write(temp.columns)
-    for index,row in temp.iterrows():
-        with cols[index % 3]:
-            if 'https' not in row['image_link']:
-                st.image(f"https://spectrum.adobe.com{row['image_link']}", caption=row['description'],use_container_width =False)
-            else:
-                st.image(row['image_link'], caption=row['description'],use_container_width =False)
-    file_name = capitalize_if_needed(st.session_state['filename'].replace(".html",""))
-    # fonts_folder = os.path.join(project_path,'fonts')
-    fonts_path = os.path.join(fonts_folder,file_name)
-    fonts = os.listdir(fonts_path)
-    #fonts = os.listdir(f'./fonts/{file_name}')
     
-  
-    if choice is not None and 'font' in choice.lower():
-        # st.write(file_name)
-        c = st.selectbox(
-        "Select the font",
-        [i for i in fonts],
-        index=None,
-        placeholder="Please select the font..."
+        if choice is not None and 'font' in choice.lower():
+            # st.write(file_name)
+            c = st.selectbox(
+            "Select the font",
+            [i for i in fonts],
+            index=None,
+            placeholder="Please select the font..."
+            
         
-       
-    )
-        if c is not None:
-            img = image_font_generation(c,file_name)
-            st.image(img)
+        )
+            if c is not None:
+                img = image_font_generation(c,file_name)
+                st.image(img)
+        
